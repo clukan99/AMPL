ac3622caddf9403301a6d315540479403e111a8a
clukan99 <clukan@butler.edu>
2021-12-03 16:38:44 -0500

    V5

--- atomsci/ddm/pipeline/feature_importance.py
+++ atomsci/ddm/pipeline/feature_importance.py
@@ -8,13 +8,19 @@ import pandas as pd
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import r2_score
 import random
+
 import json
+import tarfile
+import os
+import glob
 
 
 from atomsci.ddm.pipeline import model_pipeline as mp
 from atomsci.ddm.pipeline import parameter_parser as parse
 from atomsci.ddm.utils.struct_utils import base_smiles_from_smiles
 from atomsci.ddm.pipeline import predict_from_model as pfm
+
+
 
 #***************************************
 def _making_dataframe(model_type, pred_df,auc_roc_list_grid,r2_list_grid,response_col):
@@ -29,8 +35,6 @@ def _making_dataframe(model_type, pred_df,auc_roc_list_grid,r2_list_grid,respons
         auc_roc_list_grid.append(auc_roc_scored)
 #***************************************
 
-    
-    
 #***************************************
 def _shuffle_columns(tempFeaturizer,j, columns, feature_list_grid):
     """
@@ -46,42 +50,34 @@ def _shuffle_columns(tempFeaturizer,j, columns, feature_list_grid):
     return tempFeaturizer
 #***************************************
 
-#def predict_feature_importance(model_path, input_df, id_col, smiles_col, response_col,n_cols = None):
+#***************************************
+def _extract_model_data(model_path):
+    #Opening the tarfile
+    tempdir = tempfile.mkdtemp()
+    model_file_path = tarfile.open(model_path, mode= 'r:gz')
+    model_file_path.extractall(path = tempdir)
+    model_file_path.close()
+    
+    #Make metadata path
+    metadata_path = os.path.join(tempdir, 'model_metadata.json')
+    json_file = open(metadata_path)
+    json_data = json.load(json_file)
+    json_file.close()
+    
+    return json_data
 
-def predict_feature_importance(model_type, model_path, input_df, id_col, smiles_col, response_col,featurizer_kind = None,n_cols = None):
+#***************************************
+def predict_feature_importance(model_path, input_df):
     """
     Loads a pretrained model from the model tarball file and runs predictions on feature importance by shuffling the feature  columns for given descriptors
     
     Args:
-        model_type (str): Input of the model type can be Reg (regression) or Class (classification).
-        
-        featurizer_kind (str): Input of the type of featurizer that can be used (moe, moe_raw, moe_scaled, moe_norm, moe_filtered, moe_scaled_filtered, moe_informative, moe_scaled, mordred_filtered, mordred_raw, rdkit_raw)
-        
- 
         
         model_path: The path to take to reach the tarball file
         
         input_df (DataFrame): The input DataFrame that contains id_col, smiles_col, response_col
         
-        id_col (str): Name of the column containing compound IDs. If none is provided, sequential IDs will be
-        generated.
-        
-        smiles_col (str): Name of the column containing SMILES strings; required.
-        
-        response_col (str): Name of an optional column containing actual response values; if it is provided,
-        the actual values will be included in the returned data frame to make it easier for you to assess performance.
-        
-        n_cols (int): This is set to none as default. If a user would like to use their own data set, they may enter in the length of their dataset. It must be noted that the feature column must be last.
-        
-        
-    Return:
-        A data frame with the name of the feature column that was shuffled and a column with the corresponding AUC_ROC or r2 score. Note, all parameters must be present for the function to work.
     """
-    #Infer the featurizer kind, model type from the tarball
-    #Set featurizer kind automatically
-    #Check input dataframe
-    #Combine the two input definitions (Pred_df and data_to_predict)
-    
     #***************************************
     tempFeaturizer = input_df.copy()
     columns = list(tempFeaturizer.columns)
@@ -92,16 +88,15 @@ def predict_feature_importance(model_type, model_path, input_df, id_col, smiles_
     #***************************************
     
     #***************************************
-    #finding the featurizing kind here
-    #file_path =
-    #file = open
-    #featurizer_kind =
-    
-    
+    #finding the featurizer kind here
+    json_data = _extract_model_data(model_path)
+    #Getting the rest of the params
+    smiles_col = json_data['training_dataset']['smiles_col']
+    id_col = json_data['training_dataset']['id_col']
+    response_col = json_data['training_dataset']['response_cols'][0]
+    featurizer_kind = json_data['descriptor_specific']['descriptor_type']
+    model_type = json_data['model_parameters']['prediction_type']
     #***************************************
-    
-    
-    
     
     #***************************************
     if featurizer_kind == 'moe'or featurizer_kind == 'moe_norm'or featurizer_kind == 'moe_filtered' or featurizer_kind == 'moe_scaled_filtered':
@@ -122,8 +117,6 @@ def predict_feature_importance(model_type, model_path, input_df, id_col, smiles_
         raise ValueError('Entered featurizer kind does not exist. Please use a feature kind already listed in atomsci/ddm/data/descriptor_sets_sources_by_descr_type.csv')
     #***************************************
     
-    
-        
     #***************************************
     i = 0
     while i <= numberOfColumns + 1:
@@ -137,9 +130,6 @@ def predict_feature_importance(model_type, model_path, input_df, id_col, smiles_
         i+=1
     #***************************************
     
-    
-    
-    
     #***************************************
     if model_type == 'regression':
         perf_df = pd.DataFrame({"Features": feature_list_grid, "r2_score": r2_list_grid})
@@ -148,5 +138,3 @@ def predict_feature_importance(model_type, model_path, input_df, id_col, smiles_
         perf_df = pd.DataFrame({"Features": feature_list_grid, "auc_roc_score": auc_roc_list_grid})
         return perf_df
     #***************************************
-
-
